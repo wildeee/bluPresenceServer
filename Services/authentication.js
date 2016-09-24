575c500b15605cdeb9dd4a45c4d58b97658c6380
Wilde <gutopicciani@yahoo.com.br>
2016-09-24 15:40:55 -0300

    Adicionando status 404 para falha de auth

--- Services/authentication.js
+++ Services/authentication.js
@@ -2,11 +2,22 @@ module.exports = function(app, routename){
 
 	app.post(routename, function(req, res){
 		console.log(req.body);
-		res.send({
-			 "id"       : 123
-			,"login"    : "prof"
-			,"password" : "pass"
+		var professores = require('./../Data/Users.json');
+		var professorAutenticado = null;
+		professores.some(function(professor){
+			if (professor.login === req.body.login && professor.password === req.body.senha){
+				professorAutenticado = professor;
+				return true;
+			}
 		});
+		console.log(professorAutenticado);
+
+		if (!professorAutenticado){
+			res.statusCode = 404;
+			res.send('Authentication failed!');
+		} else {
+			res.send(professorAutenticado);
+		}
 	})
 
 }
